c70207becf5151f3f401f1aca51fbec0b0da37df
Abhinav Jangda <abhijangda@gmail.com>
2021-08-17 16:53:34 +0000

    add custom_collective device functions

---
+++ src/collectives/device/custom_collective.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+ * Copyright (c) 2015-2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+
+ #include "sccl_interpreter.h"
+ 
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollective, NCCL_ALGO_SCCL, NCCL_PROTO_SIMPLE, FUNC, T, UNROLL> : public scclFunction2D<T, SimpleWrapper2D<FUNC, T, UNROLL>, false> {};
+
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollective, NCCL_ALGO_SCCL, NCCL_PROTO_LL128, FUNC, T, UNROLL> : public scclFunction<T, LL128Wrapper<FUNC, T>> {};
+
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollective, NCCL_ALGO_SCCL, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+
+//FIXME: Find a way to remove below declarations for RING, TREE, and COLLNET.
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollective, NCCL_ALGO_RING, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollective, NCCL_ALGO_TREE, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollective, NCCL_ALGO_COLLNET, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+
+
+//Information Kernels
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollectiveInfo, NCCL_ALGO_SCCL, NCCL_PROTO_SIMPLE, FUNC, T, UNROLL> : public scclFunction2D<T, SimpleWrapper2DInfo<FUNC, T, UNROLL>, false> {};
+
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollectiveInfo, NCCL_ALGO_SCCL, NCCL_PROTO_LL128, FUNC, T, UNROLL> : public scclFunction<T, LL128Wrapper<FUNC, T>> {};
+
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollectiveInfo, NCCL_ALGO_SCCL, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+
+//FIXME: Find a way to remove below declarations for RING, TREE, and COLLNET.
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollectiveInfo, NCCL_ALGO_RING, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollectiveInfo, NCCL_ALGO_TREE, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
+template<class FUNC, typename T, int UNROLL>
+class ncclFunction<ncclFuncCustomCollectiveInfo, NCCL_ALGO_COLLNET, NCCL_PROTO_LL, FUNC, T, UNROLL> : public scclFunction<T, LLWrapper<FUNC, T>> {};
