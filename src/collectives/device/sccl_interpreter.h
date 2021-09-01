51889ea55fb79cb2e56b114c2a2ad56024414ca5
Saeed Maleki <saemal@microsoft.com>
2021-09-01 08:16:29 +0000

    low latency allreduce

--- src/collectives/device/sccl_interpreter.h
+++ src/collectives/device/sccl_interpreter.h
@@ -19,75 +19,65 @@ class scclFunction {
   public:
     __device__ void run(struct ncclWorkElem* args, int sizeMultiplier) {
       struct ncclDevComm* comm = args->comm;
-      struct scclAlgorithm* scclAlgo = &comm->scclAlgo;
       const int tid = threadIdx.x;
       const int sync_tid = args->nThreads-1; // last thread is most likely not doing anthing and used for SCCL cross thread synchronization
       const int bid = blockIdx.x;
-      struct scclThreadBlock* scclTB = &scclAlgo->scclTB[bid];
-      const int channelId = scclTB->channelId;
-      struct ncclChannel* channel = comm->channels+channelId;
+      const int bdim = blockDim.x;
+      struct ncclChannel* channel = comm->channels;
 
       // Compute pointers
       T * thisInput = (T*)args->sendbuff;
-      T * thisOutput = (T*)args->recvbuff;
+
       T * thisScratch = (T*)args->scratchbuff;
-      int recvPeer = scclTB->recvpeer;
-      int sendPeer = scclTB->sendpeer;
+      int myRank = channel->ring.devUserRanks[0];
+      int peer = (myRank > bid) ? bid : bid+1;
+      PRIMS_WRAPPER prims{args, tid, &peer, &peer, thisInput, channel};
 
-      PRIMS_WRAPPER prims{args, tid, &recvPeer, &sendPeer, thisOutput, channel};
-
-      const ssize_t loopSize = (ssize_t)prims.chunkSize;
       const ssize_t size = args->coll.count;
-      const ssize_t sizePerScclChunk = (size*sizeMultiplier)/scclAlgo->nchunksPerLoop;
-      uint32_t scclMaxAllowedCount = args->scclMaxAllowedCount;
+      const int sizePerChunk = size/8;
+      size_t chunkOffset = prims.initIter(sizePerChunk, 0);
 
       // sccl flags all start out with 0. this is used as a part of the flag to make sure different work items deal with different synchronization flags
       // this still needs more work. when we make a way around the queue, the flag might have been set to undesired values. will be fixed in subsequent versions.
       const int workIndex = args->index+1;
       volatile struct scclFlag* scclFlags = comm->scclAlgo.flags;
 
-      for (ssize_t gridOffset = 0, iter = 0; gridOffset < sizePerScclChunk; gridOffset += loopSize, iter++) {
-        size_t chunkOffset = prims.initIter(sizePerScclChunk, gridOffset);
-        ssize_t srcoffset, dstoffset;
-        T* srcPointer, * dstPointer;
-        for (int i = 0; i < scclTB->nsteps; i++){
-          struct scclTransfer* sccltran = &scclTB->transfers[i];
-          // first wait if there is a dependence
-          int8_t dependentBid = sccltran->dependentBid;
-          int8_t dependentStep = sccltran->dependentStep;
-          if (sccltran->dependentBid >= 0){
-              if (tid == sync_tid){
-              uint64_t goalFlag = COMPUTE_FLAG(workIndex, iter, dependentStep);
-              while ((scclFlags + dependentBid)->flag < goalFlag){};
-              }
-              __syncthreads();
-          }
-
-          srcPointer = (sccltran->srcbuffer == SCCL_INPUT_BUFFER) ? thisInput : ((sccltran->srcbuffer == SCCL_OUTPUT_BUFFER) ? thisOutput : thisScratch);
-          dstPointer = (sccltran->dstbuffer == SCCL_INPUT_BUFFER) ? thisInput : ((sccltran->dstbuffer == SCCL_OUTPUT_BUFFER) ? thisOutput : thisScratch);
-          int count = sccltran->count;
-          for (int c = 0; c < count; c += scclMaxAllowedCount) {
-            srcoffset = chunkOffset + (ssize_t) (sccltran->srcoffset+c) * sizePerScclChunk;
-            dstoffset = chunkOffset + (ssize_t) (sccltran->dstoffset+c) * sizePerScclChunk;
-            int thisCount = min(scclMaxAllowedCount, count-c);
-            if (sccltran->type == SCCL_SEND)
-                prims.send(srcPointer + srcoffset, dstoffset, thisCount);
-            else if (sccltran->type == SCCL_RECV)
-                prims.recv(dstPointer + dstoffset, dstoffset, thisCount);
-            else if (sccltran->type == SCCL_RECV_REDUCE_COPY)
-                prims.recvReduceCopy(srcPointer + srcoffset, dstPointer + dstoffset, thisCount);
-            else if (sccltran->type == SCCL_REDUCE)
-                prims.reduce(srcPointer + srcoffset, dstPointer + dstoffset, thisCount);
-          }
-          if (sccltran->has_dependence)
-            __syncthreads();
-          if (tid == sync_tid && sccltran->has_dependence){
-            __threadfence();
-            uint64_t curFlag = COMPUTE_FLAG(workIndex, iter, i);
-            scclFlags[bid].flag = curFlag;
-          }
-        }
+      prims.send(thisInput+peer*sizePerChunk, peer*sizePerChunk, 1);
+      prims.recv(thisScratch+bid*sizePerChunk, bid*sizePerChunk, 1);
+      if (tid == sync_tid){
+        __threadfence();
+        uint64_t curFlag = COMPUTE_FLAG(workIndex, 0, 0);
+        scclFlags[bid].flag = curFlag;
       }
+      if (tid < 7){
+        uint64_t goalFlag = COMPUTE_FLAG(workIndex, 0, 0);
+        while ((scclFlags + tid)->flag < goalFlag){};
+      }
+      __syncthreads();
+
+      const int nthreads = args->nThreads;
+      for (int j = bid*bdim+tid; j < sizePerChunk; j += nthreads*7){
+        T t = thisInput[myRank*sizePerChunk+j];
+        for (int i = 0; i < 7; i++){
+          T c = thisScratch[i*sizePerChunk+j];
+          t = (float) c + (float) t;
+        }
+        thisInput[myRank*sizePerChunk+j] = t;
+      }
+      __syncthreads();
+
+      if (bid*bdim < sizePerChunk && tid == sync_tid){
+        __threadfence();
+        uint64_t curFlag = COMPUTE_FLAG(workIndex, 0, 1);
+        scclFlags[bid].flag = curFlag;
+      }
+      if (tid*bdim < sizePerChunk){
+        uint64_t goalFlag = COMPUTE_FLAG(workIndex, 0, 1);
+        while ((scclFlags + tid)->flag < goalFlag){};
+      }
+      __syncthreads();
+      prims.send(thisInput+myRank*sizePerChunk, myRank*sizePerChunk, 1);
+      prims.recv(thisInput+peer*sizePerChunk, peer*sizePerChunk, 1);
     }
 };
 
